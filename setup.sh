7e78a1de55ba2e1a0fbc9a4c5ef4c85c3834ed11
Bhavesh Bhatt <20332240+bhattbhavesh91@users.noreply.github.com>
2021-05-02 15:53:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
+# Modified from
+# https://github.com/MaartenGr/streamlit_guide/blob/master/Procfile
+# and
+# https://github.com/MaartenGr/streamlit_guide/blob/master/setup.sh
+
 mkdir -p ~/.streamlit/
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
-
-
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# \n\
-# " > ~/.streamlit/config.toml
